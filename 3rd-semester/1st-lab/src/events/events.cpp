1b883a8a6bf34bae501cee80dc61aea98da6f9e1
theseChains <rasim0103@gmail.com>
2022-09-18 15:56:26 +0300

    Separate event handler functions

---
+++ 3rd-semester/1st-lab/src/events/events.cpp
@@ -0,0 +1,58 @@
+#include "events.h"
+
+void checkForCircleShapeCreation(std::vector<std::unique_ptr<Circle>>& circlesToRender)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+    {
+        Circle defaultCircle{};
+        defaultCircle.show(circlesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+    {
+        Circle firstConstructorCircle{ 120.0f, 40.0f, 90.0f, sf::Color::Yellow };
+        firstConstructorCircle.show(circlesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::V))
+    {
+        Circle secondConstructorCircle{ 200.0f, 130.0f, 120.0f, util::secondCircleColorComponents };
+        secondConstructorCircle.show(circlesToRender);
+    }
+}
+
+void checkForRectangleShapeCreation(std::vector<std::unique_ptr<Rectangle>>& rectanglesToRender)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+    {
+        Rectangle defaultRectangle{};
+        defaultRectangle.show(rectanglesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+    {
+        Rectangle firstConstructorRectangle{ 55.0f, 95.0f, 100.0f, 80.0f, sf::Color::Green };
+        firstConstructorRectangle.show(rectanglesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::V))
+    {
+        Rectangle secondConstructorRectangle{ 490.0f, 200.0f, 70.0f, 130.0f, util::secondRectangleColorComponents };
+        secondConstructorRectangle.show(rectanglesToRender);
+    }
+}
+
+void checkForLineShapeCreation(std::vector<std::unique_ptr<Line>>& linesToRender)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+    {
+        Line defaultLine{};
+        defaultLine.show(linesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+    {
+        Line firstConstructorLine{ 380.0f, 300.0f, 85.0f, sf::Color::White };
+        firstConstructorLine.show(linesToRender);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::V))
+    {
+        Line secondConstructorLine{ 45.0f, 210.0f, 40.0f, util::secondLineColorComponents };
+        secondConstructorLine.show(linesToRender);
+    }
+}
---
+++ 3rd-semester/1st-lab/src/events/events.h
@@ -0,0 +1,16 @@
+#ifndef EVENTS_UTILITY_FUNCTIONS
+#define EVENTS_UTILITY_FUNCTIONS
+
+#include <memory>
+
+#include "../util.h"
+
+#include "../shapes/Circle.h"
+#include "../shapes/Rectangle.h"
+#include "../shapes/Line.h"
+
+void checkForCircleShapeCreation(std::vector<std::unique_ptr<Circle>>& circlesToRender);
+void checkForRectangleShapeCreation(std::vector<std::unique_ptr<Rectangle>>& rectanglesToRender);
+void checkForLineShapeCreation(std::vector<std::unique_ptr<Line>>& linesToRender);
+
+#endif

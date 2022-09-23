c6e1f6de36c82cdb800471f4dd1031fb131cc321
theseChains <rasim0103@gmail.com>
2022-09-23 19:27:09 +0300

    Fix array render function

---
+++ 3rd-semester/1st-lab/src/render/Renderer.cpp
@@ -0,0 +1,39 @@
+#include "Renderer.h"
+
+Renderer::Renderer(sf::RenderWindow& window) : m_window{ window }
+{}
+
+template <typename ShapeType>
+void Renderer::renderShapes(std::array<std::unique_ptr<ShapeType>, 3> &shapesToRender)
+{
+    for (const auto& shape : shapesToRender)
+    {
+        if (shape.get() != nullptr && shape.get()->isShown())
+        {
+            m_window.get().draw(shape.get()->getSprite());
+        }
+    }
+}
+
+template void Renderer::renderShapes(std::array<std::unique_ptr<Circle>, 3>&);
+template void Renderer::renderShapes(std::array<std::unique_ptr<Rectangle>, 3>&);
+template void Renderer::renderShapes(std::array<std::unique_ptr<Line>, 3>&);
+
+template <typename ShapeType>
+void Renderer::renderShapeArrays(std::vector<std::array<ShapeType, 3>>& arraysToRender)
+{
+    for (const auto& shapeArray : arraysToRender)
+    {
+        for (const auto& shape : shapeArray)
+        {
+            if (shape.isShown())
+            {
+                m_window.get().draw(shape.getSprite());
+            }
+        }
+    }
+}
+
+template void Renderer::renderShapeArrays(std::vector<std::array<Circle, 3>>&);
+template void Renderer::renderShapeArrays(std::vector<std::array<Rectangle, 3>>&);
+template void Renderer::renderShapeArrays(std::vector<std::array<Line, 3>>&);
---
+++ 3rd-semester/1st-lab/src/render/Renderer.h
@@ -0,0 +1,32 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <SFML/Graphics.hpp>
+
+#include <array>
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "../util.h"
+
+#include "../shapes/Circle.h"
+#include "../shapes/Rectangle.h"
+#include "../shapes/Line.h"
+
+class Renderer
+{
+public:
+    std::reference_wrapper<sf::RenderWindow> m_window;
+
+public:
+    Renderer(sf::RenderWindow& window);
+
+    template <typename ShapeType>
+    void renderShapes(std::array<std::unique_ptr<ShapeType>, 3>& shapesToRender);
+
+    template <typename ShapeType>
+    void renderShapeArrays(std::vector<std::array<ShapeType, 3>>& arraysToRender);
+};
+
+#endif

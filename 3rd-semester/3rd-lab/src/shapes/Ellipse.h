a907bd5f87b6e3a3bf5a3ea6b2e1dc573ed869db
theseChains <rasim0103@gmail.com>
2022-10-02 01:44:37 +0300

    Add Ellipse class header file

---
+++ 3rd-semester/3rd-lab/src/shapes/Ellipse.h
@@ -0,0 +1,32 @@
+#ifndef ELLIPSE_PRIMITIVE_H
+#define ELLIPSE_PRIMITIVE_H
+
+#include <SFML/Graphics.hpp>
+
+#include "Circle.h"
+
+class Ellipse : public Circle
+{
+public:
+    Ellipse();
+    Ellipse(float topLeftX, float topLeftY, float radius, const sf::Color& color);
+    Ellipse(float topLeftX, float topLeftY, float radius, const std::array<int, 4>& colorComponents);
+
+    ToShow show(bool modifyVisibility = true) override;
+
+    void moveTo(float offsetX, float offsetY) override;
+
+    void rotateByNinetyDegrees();
+
+    bool isShown() const;
+
+private:
+    // the second radius
+    float m_semiaxis{};
+
+    bool m_isShown{};
+
+    sf::CircleShape createSprite() const;
+};
+
+#endif

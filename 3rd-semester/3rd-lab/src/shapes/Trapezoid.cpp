6dd3d5cba4372342114b9ea110d6569dc7ff3747
theseChains <rasim0103@gmail.com>
2022-10-04 19:40:30 +0300

    Add Trapezoid class implementation

---
+++ 3rd-semester/3rd-lab/src/shapes/Trapezoid.cpp
@@ -0,0 +1,90 @@
+#include "Trapezoid.h"
+
+Trapezoid::Trapezoid()
+    : Quadrangle{ rnd::getFloat(util::guiWidth, util::windowWidth - 100),
+        rnd::getFloat(0, util::windowHeight - 100), sf::Color::Magenta },
+    m_topBaseLength{ 120.0f },
+    m_bottomBaseLength{ 180.0f }
+{
+    std::cout << "Trapezoid object created\n\n";
+}
+
+Trapezoid::Trapezoid(float mainPointX, float mainPointY, float topBaseLength,
+            float bottomBaseLength, float height, const sf::Color& color)
+    : Quadrangle{ mainPointX, mainPointY, color },
+    m_topBaseLength{ topBaseLength },
+    m_bottomBaseLength{ bottomBaseLength }
+{
+    std::cout << "Trapezoid object created\n\n";
+}
+
+Trapezoid::Trapezoid(float mainPointX, float mainPointY, float topBaseLength,
+            float bottomBaseLength, float height, const std::array<int, 4>& colorComponents)
+    : Quadrangle{ mainPointX, mainPointY, colorComponents },
+    m_topBaseLength{ topBaseLength },
+    m_bottomBaseLength{ bottomBaseLength }
+{
+    std::cout << "Trapezoid object created\n\n";
+}
+
+Figure::ToShow Trapezoid::show(bool modifyVisibility)
+{
+    if (modifyVisibility)
+    {
+        if (m_isShown)
+        {
+            m_isShown = false;
+
+            return ToShow{};
+        }
+        else
+        {
+            m_isShown = true;
+
+            return ToShow{ sf::ConvexShape{ createSprite() } };
+        }
+    }
+    // for rendering
+    else
+    {
+        if (m_isShown)
+        {
+            return ToShow{ sf::ConvexShape{ createSprite() } };
+        }
+        else
+        {
+            return ToShow{};
+        }
+    }
+}
+
+void Trapezoid::moveTo(float offsetX, float offsetY)
+{
+    if (m_isShown)
+    {
+        show();
+
+        m_vertex.setX(m_vertex.getX() + offsetX);
+        m_vertex.setY(m_vertex.getY() + offsetY);
+
+        show();
+    }
+}
+
+sf::ConvexShape Trapezoid::createSprite() const
+{
+    sf::ConvexShape trapezoidSprite{};
+
+    trapezoidSprite.setPointCount(4);
+    trapezoidSprite.setPoint(0, sf::Vector2f{ m_vertex.getX(), m_vertex.getY() });
+    trapezoidSprite.setPoint(1, sf::Vector2f{ m_vertex.getX() + m_topBaseLength, m_vertex.getY() });
+
+    float baseDifference{ std::abs(m_bottomBaseLength - m_topBaseLength) };
+    trapezoidSprite.setPoint(2, sf::Vector2f{ m_vertex.getX() + m_topBaseLength +
+            baseDifference / 2, m_vertex.getY() + m_height });
+    trapezoidSprite.setPoint(3, sf::Vector2f{m_vertex.getX() + baseDifference / 2,
+            m_vertex.getY() + m_height });
+    trapezoidSprite.setFillColor(m_color);
+
+    return trapezoidSprite;
+}

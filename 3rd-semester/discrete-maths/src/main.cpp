fbc013a0c1e972e6ff4d41e8558fa498f4ed8d53
theseChains <rasim0103@gmail.com>
2022-12-11 20:21:07 +0300

    Reimplement main function

--- 3rd-semester/discrete-maths/src/main.cpp
+++ 3rd-semester/discrete-maths/src/main.cpp
@@ -1,30 +1,9 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
+#include "Application.h"
 
 int main()
 {
-	sf::RenderWindow window{ sf::VideoMode{ 800, 600}, "assignment" };
-	window.setFramerateLimit(60);
-
-	sf::CircleShape circle{};
-	circle.setRadius(100.0f);
-	circle.setPosition(10.0f, 10.0f);
-	circle.setFillColor(sf::Color::Blue);
-
-	while (window.isOpen())
-	{
-		sf::Event event{};
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(circle);
-		window.display();
-	}
+	Application application{};
+	application.run();
 
 	return 0;
 }

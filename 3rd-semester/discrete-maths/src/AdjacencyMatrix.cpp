1ae799df656b006403fe95842d15c45696e863cb
theseChains <rasim0103@gmail.com>
2022-12-12 13:28:04 +0300

    Render the adjacency matrix

--- 3rd-semester/discrete-maths/src/AdjacencyMatrix.cpp
+++ 3rd-semester/discrete-maths/src/AdjacencyMatrix.cpp
@@ -1,13 +1,14 @@
 #include "AdjacencyMatrix.h"
 
+#include <iostream>
+#include <string>
+
 AdjacencyMatrix::AdjacencyMatrix() : m_matrix{}
 {
-	m_font.loadFromFile("../res/sansation.ttf");
+	m_font.loadFromFile("../res/FiraMono-Medium.otf");
 
-	m_topText.setFont(m_font);
-	m_topText.setString("Adjacency matrix");
-	m_topText.setCharacterSize(25);
-	m_topText.setFillColor(sf::Color{ 200, 200, 255 });
+	initializeTopText();
+	initializeMatrixText();
 }
 
 void AdjacencyMatrix::handleEvent()
@@ -15,7 +16,42 @@ void AdjacencyMatrix::handleEvent()
 
 }
 
-void AdjacencyMatrix::draw(sf::RenderWindow& window)
+void AdjacencyMatrix::draw(sf::RenderWindow& window) const
 {
 	window.draw(m_topText);
+
+	for (const auto& text : m_matrixText)
+		window.draw(text);
+}
+
+void AdjacencyMatrix::initializeTopText()
+{
+	m_topText.setFont(m_font);
+	m_topText.setString("   Adjacency matrix");
+	m_topText.setCharacterSize(25);
+	m_topText.setFillColor(sf::Color{ 200, 200, 255 });
+}
+
+void AdjacencyMatrix::initializeMatrixText()
+{
+	for (auto& text : m_matrixText)
+	{
+		text.setFont(m_font);
+		text.setCharacterSize(15);
+	}
+
+	// there has to be an easier way...
+	m_matrixText[0].setString("    v0 v1 v2 v3 v4 v5 v6 v7 v8 v9");
+	m_matrixText[0].setPosition(0.0f, 30.0f);
+
+	for (int i{ 1 }; i < m_matrixText.size(); ++i)
+	{
+		std::string matrixRowString{ "   " };
+		for (int j{ 0 }; j < 10; ++j)
+		{
+			matrixRowString += std::to_string(m_matrix[i - 1][j]) + "  ";
+		}
+		m_matrixText[i].setString("v" + std::to_string(i - 1) + matrixRowString);
+		m_matrixText[i].setPosition(0.0f, 25.0f * (i + 1));
+	}
 }
--- 3rd-semester/discrete-maths/src/AdjacencyMatrix.h
+++ 3rd-semester/discrete-maths/src/AdjacencyMatrix.h
@@ -1,16 +1,6 @@
 #ifndef ADJACENCY_MATRIX_H
 #define ADJACENCY_MATRIX_H
 
-// the adjacency matrix should have a fixed size... otherwise it will just be a disaster...
-// i'm thinking of restricting the max amount of vertices to 10, should be a good size i think
-// so the number of rows and columns is 10
-
-// so what is an adjacency matrix exactly...
-// the adjacency matrix consists of a text on the top, which is 16 characters long
-// then there is the vertices row, the matrix itself is 10x10, each nubmer (0 or 1) being 15x15 px
-
-// hmm.. i think we could make it a little bigger..
-
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Text.hpp>
@@ -23,13 +13,18 @@ public:
 	AdjacencyMatrix();
 
 	void handleEvent();
-	void draw(sf::RenderWindow& window);
+	void draw(sf::RenderWindow& window) const;
+
+private:
+	void initializeTopText();
+	void initializeMatrixText();
 
 private:
 	std::array<std::array<bool, 10>, 10> m_matrix{};
 
-	sf::Text m_topText{};
 	sf::Font m_font{};
+	sf::Text m_topText{};
+	std::array<sf::Text, 11> m_matrixText{};
 };
 
 #endif

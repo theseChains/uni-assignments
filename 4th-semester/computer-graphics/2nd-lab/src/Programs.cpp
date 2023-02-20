8cdf2dc5eb33ca398a4227e63b56eb9c27a5ac60
theseChains <rasim0103@gmail.com>
2023-02-20 10:44:00 +0300

    Implement triangle rotation

--- 4th-semester/computer-graphics/2nd-lab/src/Programs.cpp
+++ 4th-semester/computer-graphics/2nd-lab/src/Programs.cpp
@@ -67,6 +67,7 @@ void runSecondProgram()
 {
 	Window window{};
 	Shader shader{ "../shaders/2dShader.vert", "../shaders/2dShader.frag" };
+	Shader triangleShader{ "../shaders/2dShader.vert", "../shaders/2dShader.frag" };
 
 	float rectangleVertices[]{
 		// first triangle
@@ -77,21 +78,55 @@ void runSecondProgram()
 		 0.5f, -0.5f, 0.0f,
 		-0.5f, -0.5f, 0.0f,
 		-0.5f,  0.5f, 0.0f
-	}; 
+	};
+	
+	float triangleVertices[]{
+		 0.25f, 0.5f, 0.0f,
+		-0.25f, 0.5f, 0.0f,
+		 0.00f, 0.9f, 0.0f
+	};
 
 	Mesh rectangle{ rectangleVertices, sizeof(rectangleVertices), 3 };
+	Mesh triangle{ triangleVertices, sizeof(triangleVertices), 3 };
+
+	double currentFrame{ glfwGetTime() };
+    double lastFrame{ currentFrame };
+    double elapsedTime{ 0.0f };
+	
+	float currentRotationAngle{ 0.0f };
 
 	while (!window.windowShouldClose())
 	{
+		currentFrame = glfwGetTime();
+		elapsedTime += currentFrame - lastFrame;
+		lastFrame = currentFrame;
+	
 		window.processInput();
 
 		glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glm::mat4 transform{ glm::mat4{ 1.0f } };
+
 		shader.use();
+		shader.setMat4("transform", transform);
 
 		glBindVertexArray(rectangle.getVAO());
 		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		triangleShader.use();
+		transform = glm::rotate(transform, glm::radians(currentRotationAngle),
+				glm::vec3{ 0.0f, 0.0f, 1.0f });
+		triangleShader.setMat4("transform", transform);
+
+		if (elapsedTime >= 1.0f)
+		{
+			currentRotationAngle += 90.0f;
+			elapsedTime = 0.0f;
+		}
+
+		glBindVertexArray(triangle.getVAO());
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		window.swapBuffers();
 		glfwPollEvents();

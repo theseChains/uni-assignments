c26e3745b27cb57ef6f355c841a730df81bd49ec
theseChains <rasim0103@gmail.com>
2023-02-22 21:35:13 +0300

    Make the dot spin in a spiral

--- 4th-semester/computer-graphics/2nd-lab/src/Programs.cpp
+++ 4th-semester/computer-graphics/2nd-lab/src/Programs.cpp
@@ -7,48 +7,34 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <algorithm>
-#include <array>
-
 #include "Mesh.h"
 #include "Shader.h"
 #include "Window.h"
 
 void runFirstProgram()
 {
-	Window window{};
+	Window window{ 1200, 720 };
 	Shader shader{ "../shaders/2dShader.vert", "../shaders/2dShader.frag" };
 
-	float vertex[]{ -0.8f, -0.8f, 0.0f };
+	float vertex[]{ 0.0f, 0.0f, 0.0f };
 	Mesh dot{ vertex, sizeof(vertex), 1 };
 
-	double currentFrame{ glfwGetTime() };
-    double lastFrame{ currentFrame };
-    double deltaTime{};
-
 	glPointSize(5.0f);
-	//glm::vec3 movementSpeed{ glm::vec3{ 0.01f } };
-	glm::vec3 startingPosition{ -0.8f, -0.8f, 0.0f };
-	glm::vec3 endPosition{ glm::vec3{ 0.0f } };
+	glm::vec3 currentTranslate{ -0.8f, 0.0f, 0.0f };
+	float currentAngle{ 0.0f };
 	while (!window.windowShouldClose())
 	{
-		currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-
 		window.processInput();
 
 		glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		float alpha{ 0.1f * static_cast<float>(deltaTime) };
-		std::clamp(alpha, 0.0f, 1.0f);
-		glm::vec3 currentPosition{ startingPosition * alpha + (1 - alpha) * endPosition };
-
 		glm::mat4 transform{ glm::mat4{ 1.0f } };
-		transform = glm::translate(transform, glm::vec3{ alpha * 10.0f });
-		transform = glm::rotate(transform, static_cast<float>(glfwGetTime()),
-				glm::vec3{ 0.0f, 0.0f, 1.0f });
+		transform = glm::rotate(transform, currentAngle, glm::vec3{ 0.0f, 0.0f, 1.0f });
+		transform = glm::translate(transform, currentTranslate);
+
+		currentTranslate.x += 0.0025f;
+		currentAngle += 0.05f;
 
 		shader.use();
 		shader.setMat4("transform", transform);
@@ -65,7 +51,7 @@ void runFirstProgram()
 
 void runSecondProgram()
 {
-	Window window{};
+	Window window{ 800, 800 };
 	Shader shader{ "../shaders/2dShader.vert", "../shaders/2dShader.frag" };
 	Shader triangleShader{ "../shaders/2dShader.vert", "../shaders/2dShader.frag" };
 
@@ -79,7 +65,7 @@ void runSecondProgram()
 		-0.5f, -0.5f, 0.0f,
 		-0.5f,  0.5f, 0.0f
 	};
-	
+
 	float triangleVertices[]{
 		 0.25f, 0.5f, 0.0f,
 		-0.25f, 0.5f, 0.0f,
@@ -92,7 +78,7 @@ void runSecondProgram()
 	double currentFrame{ glfwGetTime() };
     double lastFrame{ currentFrame };
     double elapsedTime{ 0.0f };
-	
+
 	float currentRotationAngle{ 0.0f };
 
 	while (!window.windowShouldClose())
@@ -100,7 +86,7 @@ void runSecondProgram()
 		currentFrame = glfwGetTime();
 		elapsedTime += currentFrame - lastFrame;
 		lastFrame = currentFrame;
-	
+
 		window.processInput();
 
 		glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
@@ -124,6 +110,8 @@ void runSecondProgram()
 			currentRotationAngle += 90.0f;
 			elapsedTime = 0.0f;
 		}
+		if (currentRotationAngle >= 360.0f)
+			currentRotationAngle = 0.0f;
 
 		glBindVertexArray(triangle.getVAO());
 		glDrawArrays(GL_TRIANGLES, 0, 3);

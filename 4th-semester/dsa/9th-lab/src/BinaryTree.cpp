611709914eab44429193879c1231902745e657de
theseChains <rasim0103@gmail.com>
2023-03-14 20:14:16 +0300

    Implement the add and print functions

--- 4th-semester/dsa/9th-lab/src/BinaryTree.cpp
+++ 4th-semester/dsa/9th-lab/src/BinaryTree.cpp
@@ -1,5 +1,8 @@
 #include "BinaryTree.h"
 
+#include <iostream>
+#include <random>
+
 namespace rnd
 {
 	std::mt19937 mt{ std::random_device{}() };
@@ -11,22 +14,47 @@ namespace rnd
 	}
 }
 
-void addToTree(TreeNode* root, int numberOfElements)
+void addToTree(TreeNode*& root, int numberOfElements)
 {
+	if (numberOfElements == 0)
+		return;
 
+	int numberOfLeftChildren{ numberOfElements / 2 };
+	int numberOfRightChildren{ numberOfElements - numberOfLeftChildren - 1 };
+
+	TreeNode* newNode{ new TreeNode{} };
+	newNode->value = rnd::getNumber(0, 99);
+	addToTree(newNode->left, numberOfLeftChildren);
+	addToTree(newNode->right, numberOfRightChildren);
+	root = newNode;
 }
 
-void printTreeInDirectOrder(TreeNode* root)
+void printTreeInDirectOrder(TreeNode* root, int level)
 {
+	if (root == nullptr)
+		return;
 
+	std::cout << std::string(level, ' ') << root->value << '\n';
+	printTreeInDirectOrder(root->left, level + 4);
+	printTreeInDirectOrder(root->right, level + 4);
 }
 
-void printTreeInSymmetricOrder(TreeNode* root)
+void printTreeInSymmetricOrder(TreeNode* root, int level)
 {
+	if (root == nullptr)
+		return;
 
+	printTreeInSymmetricOrder(root->left, level + 4);
+	std::cout << std::string(level, ' ') << root->value << '\n';
+	printTreeInSymmetricOrder(root->right, level + 4);
 }
 
-void printTreeInBackwardSymmetricOrder(TreeNode* root)
+void printTreeInBackwardSymmetricOrder(TreeNode* root, int level)
 {
+	if (root == nullptr)
+		return;
 
+	printTreeInBackwardSymmetricOrder(root->right, level + 4);
+	std::cout << std::string(level, ' ') << root->value << '\n';
+	printTreeInBackwardSymmetricOrder(root->left, level + 4);
 }

711b8fe20b084418cbb554df90fa68ec0719eafc
theseChains <rasim0103@gmail.com>
2023-03-24 19:56:32 +0300

    Update the menu

--- 4th-semester/dsa/12th-lab/src/Menu.cpp
+++ 4th-semester/dsa/12th-lab/src/Menu.cpp
@@ -4,12 +4,25 @@
 #include <iostream>
 #include <random>
 
+namespace rnd
+{
+	std::mt19937 mt{ std::random_device{}() };
+
+	int get(int min, int max)
+	{
+		std::uniform_int_distribution range{ min, max };
+		return range(mt);
+	}
+}
+
 void printMenu()
 {
 	std::cout << "\n1:  add element to tree\n";
 	std::cout << "2:  search for element in tree\n";
 	std::cout << "3:  print tree\n";
-	std::cout << "4:  destroy tree\n";
+	std::cout << "4:  remove element from tree\n";
+	std::cout << "5:  destroy tree\n";
+	std::cout << "6:  add multiple elements\n";
 	std::cout << "-1: exit\n";
 }
 
@@ -22,17 +35,23 @@ int getNumber()
 
 void handleTreeAddition(TreeNode*& root)
 {
+	std::cout << "\n0: use recursive algorithm\n1: use non-recursive algorithm\n";
+	int option{ getNumber() };
 	std::cout << "\nenter new value: ";
 	int newValue{ getNumber() };
-	addToTree(root, newValue);
+	if (option == 0)
+		addToTree(root, newValue);
+	else if (option == 1)
+		addToTreeRecursive(root, newValue);
+	else
+		std::cout << "\nincorrect option\n";
 }
 
 void handleTreeSearch(TreeNode* root)
 {
 	std::cout << "\nenter value to find: ";
 	int valueToFind{ getNumber() };
-	TreeNode* found{};
-	findElementInTree(root, found, valueToFind);
+	TreeNode* found{ findElementInTree(root, valueToFind) };
 	if (found)
 		std::cout << "\nvalue " << valueToFind << " was found in the tree\n";
 	else
@@ -61,6 +80,21 @@ void handleTreePrint(const TreeNode* root)
 	printTreeInBackwardSymmetricOrder(root);
 }
 
+void handleTreeRemoval(TreeNode*& root)
+{
+	std::cout << "\nenter value to remove: ";
+	int valueToRemove{ getNumber() };
+	removeFromTree(root, valueToRemove);
+}
+
+void handleTreeCreation(TreeNode*& root)
+{
+	std::cout << "\nenter number of elements to add: ";
+	int numberOfElementsToAdd{ getNumber() };
+	for (int i{ 0 }; i < numberOfElementsToAdd; ++i)
+		addToTree(root, rnd::get(0, 99));
+}
+
 void handleCommand(TreeNode*& root, int command)
 {
 	switch (command)
@@ -75,7 +109,13 @@ void handleCommand(TreeNode*& root, int command)
 			handleTreePrint(root);
 			break;
 		case 4:
+			handleTreeRemoval(root);
+			break;
+		case 5:
 			handleTreeDestruction(root);
+			break;
+		case 6:
+			handleTreeCreation(root);
 			break;
 	}
 }

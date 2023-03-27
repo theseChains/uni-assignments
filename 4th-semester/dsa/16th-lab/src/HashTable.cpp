6fff4c8d2934df55f8231e50932e4117c45a4b91
theseChains <rasim0103@gmail.com>
2023-03-27 14:09:52 +0300

    Implement the addition and printing functions

---
+++ 4th-semester/dsa/16th-lab/src/HashTable.cpp
@@ -0,0 +1,32 @@
+#include "HashTable.h"
+
+#include <iostream>
+#include <ranges>
+
+int hashFunction(const std::string& value)
+{
+	int index{};
+	for (const auto& character : value)
+		index += static_cast<int>(character);
+
+	return (index % 10);
+}
+
+void addToTable(HashTable& table, const std::string& newValue)
+{
+	if (std::ranges::find(config::keys, newValue) == std::end(table.array))
+	{
+		std::cout << "\ncouldn't find key " << newValue << " in the list of keys\n";
+		return;
+	}
+
+	int keyIndex{ hashFunction(newValue) };
+	table.array[keyIndex] = newValue;
+}
+
+void printTable(const HashTable& table)
+{
+	for (const auto& element : table.array)
+		std::cout << element << ' ';
+	std::cout << '\n';
+}

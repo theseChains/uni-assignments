b91be866ca9ff4cee90866a9d0d24823aa48d981
theseChains <rasim0103@gmail.com>
2023-03-27 14:10:32 +0300

    Update the menu

---
+++ 4th-semester/dsa/16th-lab/src/Menu.cpp
@@ -0,0 +1,84 @@
+#include "Menu.h"
+#include "HashTable.h"
+
+#include <iostream>
+#include <random>
+
+namespace rnd
+{
+	std::mt19937 mt{ std::random_device{}() };
+
+	int get(int min, int max)
+	{
+		std::uniform_int_distribution range{ min, max };
+		return range(mt);
+	}
+}
+
+void printMenu()
+{
+	std::cout << "\n1:  add key to table\n";
+	std::cout << "3:  print table\n";
+	std::cout << "-1: exit\n";
+}
+
+int getNumber()
+{
+	int number{};
+	std::cin >> number;
+	return number;
+}
+
+std::string getString()
+{
+	std::string string{};
+	std::cin >> string;
+	return string;
+}
+
+void hanldeTableAddition(HashTable& table)
+{
+	std::cout << "\nenter key value to add: ";
+	std::string value{ getString() };
+	addToTable(table, value);
+}
+
+void handleTablePrinting(const HashTable& table)
+{
+	printTable(table);
+	std::cout << '\n';
+}
+
+void handleCommand(HashTable& table, int command)
+{
+	switch (command)
+	{
+		case 1:
+			hanldeTableAddition(table);
+			break;
+		case 2:
+			break;
+		case 3:
+			handleTablePrinting(table);
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+	}
+}
+
+void runMenuLoop()
+{
+	HashTable table{};
+	for (int i{ 0 }; i < constants::maxSize; ++i)
+		table.array[i] = "EMPTY";
+
+	int command{};
+	while (command != -1)
+	{
+		printMenu();
+		command = getNumber();
+		handleCommand(table, command);
+	}
+}

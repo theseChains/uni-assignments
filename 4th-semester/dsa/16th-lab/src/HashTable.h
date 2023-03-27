d2da5134251b54bb651cd7a6da0827547c43f839
theseChains <rasim0103@gmail.com>
2023-03-27 14:09:32 +0300

    Add a list of keys and implement the hash table structure

---
+++ 4th-semester/dsa/16th-lab/src/HashTable.h
@@ -0,0 +1,36 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <array>
+#include <string>
+
+namespace constants
+{
+	inline constexpr int maxSize{ 10 };
+}
+
+namespace config
+{
+	inline std::array<std::string, constants::maxSize> keys{
+		"auto",
+		"union",
+		"constexpr",
+		"volatile",
+		"explicit",
+		"friend",
+		"inline",
+		"virtual",
+		"for",
+		"template"
+	};
+}
+
+struct HashTable
+{
+	std::string array[constants::maxSize]{};
+};
+
+void addToTable(HashTable& table, const std::string& newValue);
+void printTable(const HashTable& table);
+
+#endif

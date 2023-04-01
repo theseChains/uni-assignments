32ca6fee50a50b1a29c64d690ac41afae1ce32bf
theseChains <rasim0103@gmail.com>
2023-04-02 01:52:27 +0300

    Implement the sorted list add function

--- 4th-semester/dsa/term-project/src/Airline.cpp
+++ 4th-semester/dsa/term-project/src/Airline.cpp
@@ -1,11 +1,70 @@
 #include "Airline.h"
 
-Airline::Airline() : m_name{ "Airline" }, m_airports{}
+#include <iostream>
+
+Airline::Airline() : m_name{ "Airline" }, m_airportHead{ nullptr }
 {
 }
 
-Airline::Airline(const std::string& name) : m_name{ name }, m_airports{}
+Airline::Airline(const std::string& name) : m_name{ name }, m_airportHead{ nullptr }
 {
+}
+
+void Airline::addAirport(const std::string& airportName)
+{
+	Airport* newAirport{ new Airport{ airportName } };
+
+	if (m_airportHead == nullptr)
+	{
+		newAirport->setNext(nullptr);
+		newAirport->setPrev(nullptr);
+		m_airportHead = newAirport;
+	}
+	// add at the beginning
+	else if (m_airportHead->getName() >= airportName)
+	{
+		newAirport->setNext(m_airportHead);
+		newAirport->setPrev(nullptr);
+		m_airportHead = newAirport;
+	}
+	else
+	{
+		Airport* current{ m_airportHead };
+		Airport* previous{ m_airportHead };
+		while (current != nullptr && current->getName() < airportName)
+		{
+			if (current->getNext() != nullptr && current->getNext()->getName() < airportName)
+				previous = previous->getNext();
+
+			current = current->getNext();
+		}
+
+		// append
+		if (current == nullptr)
+		{
+			newAirport->setNext(nullptr);
+			newAirport->setPrev(previous);
+			previous->setNext(newAirport);
+		}
+		// add before current
+		else
+		{
+			newAirport->setNext(current);
+			newAirport->setPrev(previous);
+			previous->setNext(newAirport);
+			current->setPrev(newAirport);
+		}
+	}
+}
+
+void Airline::showAirports() const
+{
+	Airport* current{ m_airportHead };
+	while (current != nullptr)
+	{
+		std::cout << "airport: " << current->getName() << '\n';
+		current = current->getNext();
+	}
 }
 
 void Airline::setName(const std::string& name)

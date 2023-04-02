54bfa2ed9b0429d1dc8131c3d31b28fd8f1816ee
theseChains <rasim0103@gmail.com>
2023-04-02 22:36:58 +0300

    Implement input validity checks

--- 4th-semester/mathematical-logic/2nd-lab/maths.pl
+++ 4th-semester/mathematical-logic/2nd-lab/maths.pl
@@ -1,5 +1,19 @@
 approx(N, X, Result) :-
+    N >= 0, X > 1, integer(N),
     approxHelper(N, X, pi / 2, Result).
+
+% input validity checks
+approx(N, _, _) :-
+    N < 0,
+    writeln("N should be non-negative").
+
+approx(N, _, _) :-
+    not(integer(N)),
+    writeln("N should be an integer").
+
+approx(_, X, _) :-
+    X =< 1,
+    writeln("X should be greater than 1").
 
 approxHelper(-1, _, Accumulator, Accumulator) :- !.
 
@@ -8,4 +22,3 @@ approxHelper(N, X, Accumulator, Result) :-
     NewAccumulator is Accumulator + ((-1) ** (N + 1)) / ((2 * N + 1) * X ** (2 * N + 1)),
     DecrementedN is N - 1,
     approxHelper(DecrementedN, X, NewAccumulator, Result).
-

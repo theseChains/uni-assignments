bacb03530c07711194775d3d12c03224236d036e
theseChains <rasim0103@gmail.com>
2023-04-23 16:31:50 +0300

    Split up the vector generation function

--- 4th-semester/dsa/15th-lab/src/Menu.cpp
+++ 4th-semester/dsa/15th-lab/src/Menu.cpp
@@ -22,7 +22,6 @@ void printMenu()
 	std::cout << "\n1:  sort using bucket sort algorithm\n";
 	std::cout << "2:  sort using generic bucket sort algorithm\n";
 	std::cout << "3:  sort using radix sort algorithm\n";
-	std::cout << "4:  generate vector\n";
 	std::cout << "-1: exit\n";
 }
 
@@ -33,79 +32,64 @@ int getNumber()
 	return number;
 }
 
-void generateVector(std::vector<int>& numbers, int option)
+void generateUniformDistribution(std::vector<int>& numbers)
 {
-	// this option stuff sucks.. whatever
-	if (option == 0)
-	{
-		int size{ static_cast<int>(numbers.size()) };
-		numbers.clear();
+	std::cout << "\nenter number of elements to generate: ";
+	int numberOfElements{ getNumber() };
+	numbers.clear();
 
-		for (int i{ 0 }; i < size; ++i)
-			numbers.push_back(i);
+	for (int i{ 0 }; i < numberOfElements; ++i)
+		numbers.push_back(i);
 
-		std::shuffle(numbers.begin(), numbers.end(), rnd::mt);
-	}
-	else
-	{
-		numbers.clear();
-		std::cout << "\nenter number of elements to generate: ";
-		int numberOfElements{ getNumber() };
-		numbers.reserve(numberOfElements);
+	std::shuffle(numbers.begin(), numbers.end(), rnd::mt);
+}
 
-		for (int i{ 0 }; i < numberOfElements; ++i)
-			numbers.push_back(rnd::get(0, numberOfElements));
-	}
+void generateVector(std::vector<int>& numbers)
+{
+	numbers.clear();
+	std::cout << "\nenter number of elements to generate: ";
+	int numberOfElements{ getNumber() };
+	numbers.reserve(numberOfElements);
+
+	for (int i{ 0 }; i < numberOfElements; ++i)
+		numbers.push_back(rnd::get(0, numberOfElements));
 }
 
 void handleSorting(std::vector<int>& numbers, int command)
 {
-	if (numbers.empty())
-	{
-		std::cout << "\nthe array is empty, cannot sort\n";
-		return;
-	}
-
-	std::cout << "\noriginal:\n";
-	printVector(numbers);
-
 	switch (command)
 	{
 		case 1:
 		{
+			generateUniformDistribution(numbers);
+
 			std::cout << "\n0: use secondary array";
 			std::cout << "\n1: don't use secondary array\n";
 			int option{ getNumber() };
-			// regenerate the vector
-			generateVector(numbers, 0);
+
+			std::cout << "\noriginal:\n";
+			printVector(numbers);
 			bucketSort(numbers, option);
 			break;
 		}
 		case 2:
+		{
+			generateVector(numbers);
+
+			std::cout << "\noriginal:\n";
+			printVector(numbers);
 			genericBucketSort(numbers);
 			break;
+		}
 		case 3:
+		{
+			generateVector(numbers);
+
+			std::cout << "\noriginal:\n";
+			printVector(numbers);
 			radixSort(numbers);
 			break;
-	}
-}
-
-void handleCommand(std::vector<int>& numbers, int command)
-{
-	switch (command)
-	{
-		case 1:
-			handleSorting(numbers, command);
-			break;
-		case 2:
-			handleSorting(numbers, command);
-			break;
-		case 3:
-			handleSorting(numbers, command);
-			break;
-		case 4:
-			generateVector(numbers, 1);
-			break;
+		}
 	}
 }
 
@@ -118,6 +102,6 @@ void runMenuLoop()
 	{
 		printMenu();
 		command = getNumber();
-		handleCommand(numbers, command);
+		handleSorting(numbers, command);
 	}
 }

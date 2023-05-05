800d68ae1a73e7fce5f1b980d7f63f56a3a0ae19
theseChains <rasim0103@gmail.com>
2023-05-05 20:10:03 +0300

    Reimplement the run menu and fix input validity checks

--- 4th-semester/mathematical-logic/2nd-lab/maths.pl
+++ 4th-semester/mathematical-logic/2nd-lab/maths.pl
@@ -1,55 +1,34 @@
 approx(N, X, Result) :-
-    N >= 0, X > 1, integer(N),
+    not(number(X)) -> writeln("X should be a number"), fail;
+    X =< 1 -> writeln("X should be greater than 1"), fail;
+    not(integer(N)) -> writeln("N should be a non-negative integer"), fail;
+    N < 0 -> writeln("N should be a non-negative integer"), fail;
     approxHelper(N, X, pi / 2, Result).
-
-% input validity checks
-approx(N, _, _) :-
-    N < 0,
-    writeln("N should be non-negative").
-
-approx(N, _, _) :-
-    not(integer(N)),
-    writeln("N should be an integer").
-
-approx(_, X, _) :-
-    X =< 1,
-    writeln("X should be greater than 1").
 
 approxHelper(-1, _, Accumulator, Accumulator) :- !.
 
 approxHelper(N, X, Accumulator, Result) :-
-    N > -1,
     NewAccumulator is Accumulator + ((-1) ** (N + 1)) / ((2 * N + 1) * X ** (2 * N + 1)),
     DecrementedN is N - 1,
     approxHelper(DecrementedN, X, NewAccumulator, Result).
 
+precise(X, Result) :-
+    not(number(X)) -> writeln("X should be a number"), fail;
+    X =< 1 -> writeln("X should be greater than 1"), fail;
+    Result is atan(X).
+
 run :-
-    repeat,
-    menu,
-    readln(Option),
-    handle(Option).
-
-menu :-
-    nl,
-    writeln("choose:"),
-    writeln("calculate - calculate absolute error for arctg(X)"),
-    writeln("exit - terminate").
-
-handle([calculate]) :-
     write("Enter N: "),
     readln(NList),
     [N | _] = NList,
     write("Enter X: "),
     readln(XList),
     [X | _] = XList,
-    StandardResult is atan(X),
-    format("standard arctg(~d)    = ~6f\n", [X, StandardResult]),
+    precise(X, StandardResult),
     approx(N, X, Result),
-    format("approximate arctg(~d) = ~6f\n", [X, Result]),
+    format("standard arctg(~f)    = ~6f\n", [X, StandardResult]),
+    format("approximate arctg(~f) = ~6f\n", [X, Result]),
     writef("(N = %d)\n", [N]),
     AbsoluteError is abs(Result - StandardResult),
     format("absolute error = ~6f\n", [AbsoluteError]),
     fail.
-
-handle([exit]) :-
-    writeln("exiting the run loop").

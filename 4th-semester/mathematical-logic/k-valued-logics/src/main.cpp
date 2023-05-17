94f0d425df11609be5d3745a2314195e5a9e236b
theseChains <rasim0103@gmail.com>
2023-05-17 21:24:10 +0300

    Update the main function

--- 4th-semester/mathematical-logic/k-valued-logics/src/main.cpp
+++ 4th-semester/mathematical-logic/k-valued-logics/src/main.cpp
@@ -2,15 +2,8 @@
 #include <iostream>
 #include <string>
 
-#include "ExpressionParser.h"
+#include "OutputFunctions.h"
 #include "UserInput.h"
-
-void printAvailableOperations()
-{
-    std::cout << "\navailable operations:\n";
-    std::cout << "raising to the power of 4: x^\n";
-    std::cout << "implication: x > y\n\n";
-}
 
 int main()
 {
@@ -20,32 +13,8 @@ int main()
     printAvailableOperations();
     std::string function{ getFunction(numberOfVariables) };
     // todo: validate function input
-
-    if (numberOfVariables == 1)
-    {
-        std::cout << std::setw(4) << 'x' << std::string(3, ' ') << '|'
-            << std::string(4, ' ') << "f(x)\n";
-        for (int currentX{ 0 }; currentX < numberOfValues; ++currentX)
-        {
-            std::cout << std::setw(4) << currentX << std::string(3, ' ') << '|'
-                << std::string(3, ' ') << std::setw(4)
-                    << evaluateFunction(function, currentX, 0, numberOfValues) << '\n';
-        }
-    }
-    else if (numberOfVariables == 2)
-    {
-        std::cout << std::setw(4) << 'x' << std::string(3, ' ') << '|' << std::setw(4)
-            << 'y' << std::string(3, ' ') << '|' << std::string(3, ' ') << "f(x,y)\n";
-        for (int currentX{ 0 }; currentX < numberOfValues; ++currentX)
-        {
-            for (int currentY{ 0 }; currentY < numberOfValues; ++currentY)
-            {
-                std::cout << std::setw(4) << currentX << std::string(3, ' ') << '|'
-                    << std::setw(4) << currentY << std::string(3, ' ') << '|' << std::setw(7)
-                        << evaluateFunction(function, currentX, currentY, numberOfValues) << '\n';
-            }
-        }
-    }
+    
+    printResultTable(function, numberOfValues, numberOfVariables);
 
     return 0;
 }

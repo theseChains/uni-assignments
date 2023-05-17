fafba6f2247ce9244451cde6b9372411c0ea25be
theseChains <rasim0103@gmail.com>
2023-05-17 20:14:27 +0300

    Print the table for functions with 1 variable

--- 4th-semester/mathematical-logic/k-valued-logics/src/main.cpp
+++ 4th-semester/mathematical-logic/k-valued-logics/src/main.cpp
@@ -1,22 +1,40 @@
+#include <iomanip>
 #include <iostream>
 #include <string>
 
+#include "ExpressionParser.h"
 #include "UserInput.h"
 
 void printAvailableOperations()
 {
     std::cout << "\navailable operations:\n";
     std::cout << "raising to the power of 4: x^\n";
-    std::cout << "implication: x => y\n\n";
+    std::cout << "implication: x > y\n\n";
 }
 
 int main()
 {
-    [[maybe_unused]] int numberOfValues{ getNumber('k') };
+    int numberOfValues{ getNumber('k') };
     int numberOfVariables{ getNumber('n') };
 
     printAvailableOperations();
     std::string function{ getFunction(numberOfVariables) };
+    // todo: validate function input
+
+    if (numberOfVariables == 1)
+    {
+        std::cout << std::setw(4) << 'x' << std::string(3, ' ') << '|'
+            << std::string(4, ' ') << "f(x)\n";
+        for (int currentX{ 0 }; currentX < numberOfValues; ++currentX)
+        {
+            std::cout << std::setw(4) << currentX << std::string(3, ' ') << '|'
+                << std::string(3, ' ') << std::setw(4)
+                    << evaluateFunction(function, currentX, 0, numberOfValues) << '\n';
+        }
+    }
+    else if (numberOfVariables == 2)
+    {
+    }
 
     return 0;
 }

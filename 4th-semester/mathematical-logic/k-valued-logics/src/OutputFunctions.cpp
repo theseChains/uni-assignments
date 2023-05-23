0732b0869fbd5af2476e2c10b263f719cb6e819b
theseChains <rasim0103@gmail.com>
2023-05-23 19:52:21 +0300

    Move normal form printing to output functions file

--- 4th-semester/mathematical-logic/k-valued-logics/src/OutputFunctions.cpp
+++ 4th-semester/mathematical-logic/k-valued-logics/src/OutputFunctions.cpp
@@ -46,3 +46,47 @@ void printMultipleVariableResults(const std::vector<int>& values, int numberOfVa
         }
     }
 }
+
+void printPcnfAnalogue(const std::vector<int>& values, int numberOfValues, int numberOfVariables)
+{
+    std::cout << "pcnf analogue: ";
+    if (numberOfVariables == 1)
+        printPcnfForSingleVariable(values, numberOfValues);
+    else if (numberOfVariables == 2)
+        printPcnfForMultipleVariables(values, numberOfValues);
+    std::cout << '\n';
+}
+
+void printPcnfForSingleVariable(const std::vector<int>& values, int numberOfValues)
+{
+    for (std::size_t i{ 0 }; i < values.size(); ++i)
+    {
+        if (values[i] == 0)
+            std::cout << "~J" << i << "(x)";
+        else if (values[i] != numberOfValues - 1)
+            std::cout << '(' << values[i] << " v ~J" << i << "(x))";
+
+        if (i != values.size() - 1 && values[i + 1] != numberOfValues - 1) {
+            std::cout << " & ";
+        }
+    }
+}
+
+void printPcnfForMultipleVariables(const std::vector<int>& values, int numberOfValues)
+{
+    int index{ 0 };
+    for (int i{ 0 }; i < numberOfValues; ++i)
+    {
+        for (int j{ 0 }; j < numberOfValues; ++j, ++index)
+        {
+            if (values[index] == 0)
+                std::cout << "(~J" << i << "(x) v ~J" << j << "(y))";
+            else if (values[index] != numberOfValues - 1)
+                std::cout << '(' << values[index] << " v ~J" << i << "(x) v ~J" << j << "(y))";
+            if (index != static_cast<int>(values.size() - 1) &&
+                    values[index + 1] != numberOfValues - 1) {
+                std::cout << " & ";
+            }
+        }
+    }
+}
--- 4th-semester/mathematical-logic/k-valued-logics/src/OutputFunctions.h
+++ 4th-semester/mathematical-logic/k-valued-logics/src/OutputFunctions.h
@@ -5,8 +5,13 @@
 #include <vector>
 
 void printAvailableOperations();
+
 void printResultTable(const std::vector<int>& values, int numberOfValues, int numberOfVariables);
 void printSingleVariableResults(const std::vector<int>& values, int numberOfValues);
 void printMultipleVariableResults(const std::vector<int>& values, int numberOfValues);
+
+void printPcnfAnalogue(const std::vector<int>& values, int numberOfValues, int numberOfVariables);
+void printPcnfForSingleVariable(const std::vector<int>& values, int numberOfValues);
+void printPcnfForMultipleVariables(const std::vector<int>& values, int numberOfValues);
 
 #endif

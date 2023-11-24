136a3c7dd946af591dbe9bb5269a276cf0421fa8
theseChains <rasim0103@gmail.com>
2023-11-24 14:22:40 +0300

    Add the main application file

---
+++ 5th-semester/databases/term-project/src/application.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get('/api/stars', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM stars');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});

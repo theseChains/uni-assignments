f0ec0cab2f64a3ea104f6b9948cf637f83ed9c00
theseChains <rasim0103@gmail.com>
2024-04-11 08:13:49 +0300

    Add error handling and journaling

--- 6th-semester/java/4th-lab/TCPClient.java
+++ 6th-semester/java/4th-lab/TCPClient.java
@@ -3,35 +3,40 @@ import java.net.*;
 import java.util.Scanner;
 
 public class TCPClient implements Runnable {
-    public static final int CLIENT_COUNT = 1;
-    public static final int READ_BUFFER_SIZE = 10;
-
     private String name = null;
     private String host = null;
     private int serverPort = 0;
 
-    public TCPClient(String clientName, String hostName, int port) {
+    private String journalFileName = null;
+
+    public TCPClient(String clientName, String hostName, int port, String fileName) {
         name = clientName;
         host = hostName;
         serverPort = port;
+        journalFileName = fileName;
     }
 
     public void run() {
-        String[] expression = { "33333333333.7+", "1.6-", "5=" };
+        String[] expression = { "3.7+", "1.6-", "4-", "5=" };
+        writeToJournal("Client is running...");
         try {
             Socket socket = new Socket(host, serverPort);
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            writeToJournal("Client connected to server: " + host + ":" + serverPort);
+
             for (String argument : expression)
             {
-                System.out.println("writing: " + argument);
+                writeToJournal("Writing string to server: " + argument);
                 writer.println(argument);
                 writer.flush();
             }
 
+            writeToJournal("Finished writing expression to server");
+
             String result = reader.readLine();
-            System.out.println("Result: " + result);
+            writeToJournal("Result returned from server: " + result + "\n");
 
             writer.close();
             reader.close();
@@ -39,9 +44,17 @@ public class TCPClient implements Runnable {
 
             Thread.yield();
         } catch (UnknownHostException e) {
-            System.err.println("Исключение: " + e.toString());
+            System.err.println("Exception: " + e.toString());
         } catch (IOException e) {
-            System.err.println("Исключение: " + e.toString());
+            System.err.println("Exception: " + e.toString());
+        }
+    }
+
+    public void writeToJournal(String entry) {
+        try (FileWriter writer = new FileWriter(journalFileName, true)) {
+            writer.write(entry + "\n");
+        } catch (IOException e) {
+            System.err.println("Error occurred while writing to the server journal: " + e.getMessage());
         }
     }
 
@@ -51,10 +64,11 @@ public class TCPClient implements Runnable {
             Scanner scanner = new Scanner(file);
             String host = scanner.nextLine();
             int serverPort = Integer.parseInt(scanner.nextLine());
+            String journalFileName = scanner.nextLine();
             scanner.close();
 
             String name = "client";
-            TCPClient client = new TCPClient(name, host, serverPort);
+            TCPClient client = new TCPClient(name, host, serverPort, journalFileName);
             Thread clientThread = new Thread(client);
             clientThread.start();
         } catch (FileNotFoundException e) {
--- 6th-semester/java/4th-lab/TCPServer.java
+++ 6th-semester/java/4th-lab/TCPServer.java
@@ -1,53 +1,71 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPServer {
-    private static final int TIME_SEND_SLEEP = 100;
-    private static final int COUNT_TO_SEND = 10;
     private ServerSocket serverSocket;
+    private String journalFileName;
 
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Incorrect number of command line arguments");
         } else {
             int port = Integer.parseInt(args[0]);
-            TCPServer tcpServer = new TCPServer(port);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the name of the server journal file: ");
+            String fileName = scanner.nextLine();
+
+            TCPServer tcpServer = new TCPServer(port, fileName);
             tcpServer.go();
         }
     }
 
-    public TCPServer(int port) {
+    public TCPServer(int port, String journalFileName) {
         try {
             serverSocket = new ServerSocket(port);
+            this.journalFileName = journalFileName;
         } catch (IOException exception) {
             System.err.println("Unable to open socket for server: " + exception.toString());
         }
     }
 
-    public void go() {
-        class Listener implements Runnable{
-            Socket socket;
+    public void writeToJournal(String entry) {
+        try (FileWriter writer = new FileWriter(journalFileName, true)) {
+            writer.write(entry + "\n");
+            // System.out.println("Entry added to server journal: " + entry);
+        } catch (IOException e) {
+            System.err.println("Error occurred while writing to the server journal: " + e.getMessage());
+        }
+    }
 
-            public Listener(Socket aSocket){
-                socket = aSocket;
+    public void go() {
+        class Listener implements Runnable {
+            Socket socket;
+            PrintWriter writer;
+
+            public Listener(Socket socket) {
+                this.socket = socket;
             }
 
-            public void run(){
+            public void run() {
                 try {
-                    System.out.println("Слушатель запущен");
+                    writeToJournal("Listener is running...");
 
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    this.writer = new PrintWriter(socket.getOutputStream(), true);
 
                     StringBuilder expression = new StringBuilder();
                     String line;
                     while ((line = reader.readLine()) != null) {
+                        writeToJournal("Read string from client: " + line);
                         if (line.endsWith("=")) {
                             expression.append(line);
-                            System.err.println("string got in listener: " + expression);
+                            writeToJournal("Expression read from client: " + expression);
 
                             double result = calculateExpression(expression.toString());
                             writer.println(result);
+                            writeToJournal("Result sent to client: " + result + "\n");
 
                             expression.setLength(0);
                         } else {
@@ -58,26 +76,37 @@ public class TCPServer {
                     writer.close();
                     reader.close();
                     socket.close();
-                } catch(IOException e) {
-                    System.err.println("Исключение: " + e.toString());
+                } catch (IOException e) {
+                    writeToJournal("IOException occurred: " + e.getMessage());
+                    writer.println("IOException: " + e.getMessage());
+                    System.err.println("Exception: " + e.toString());
+                } catch (ArithmeticException e) {
+                    System.err.println("Overflow or underflow occurred");
+                    writeToJournal("ArithmeticException occurred: " + e.getMessage());
+                    writer.println("ArithmeticException: " + e.getMessage());
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid number format in expression");
+                    writeToJournal("NumberFormatException occurred: " + e.getMessage());
+                    writer.println("NumberFormatException: " + e.getMessage());
                 }
             }
         }
 
-        System.out.println("Сервер запущен...");
+        System.out.println("Server is running...");
+        writeToJournal("Server is running...");
         while (true) {
             try {
                 Socket socket = serverSocket.accept();
                 Listener listener = new Listener(socket);
                 Thread thread = new Thread(listener);
                 thread.start();
-            } catch(IOException e) {
-                System.err.println("Исключение: " + e.toString());
+            } catch (IOException e) {
+                System.err.println("Exception: " + e.toString());
             }
         }
     }
 
-    public double calculateExpression(String expression) {
+    public double calculateExpression(String expression) throws ArithmeticException, NumberFormatException {
         String removedLastChar = expression.substring(0, expression.length() - 1);
         String[] tokens = removedLastChar.split("[\\+\\-]");
 

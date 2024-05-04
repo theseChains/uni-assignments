6e09fb1ce55b74f142f6ffc6ee3fc976e1eb9310
theseChains <rasim0103@gmail.com>
2024-05-04 20:19:30 +0300

    Add a stylesheet theme for the application

--- 6th-semester/software-architecture/src/main.cpp
+++ 6th-semester/software-architecture/src/main.cpp
@@ -3,10 +3,15 @@
 #include "View/ApplicationView.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication application{ argc, argv };
+
+    QFile styleSheetFile{ "../res/darkstyle.qss" };
+    styleSheetFile.open(QFile::ReadOnly);
+    application.setStyleSheet(QLatin1String{ styleSheetFile.readAll() });
 
     polyclinic::ApplicationModel model{};
     polyclinic::ApplicationController controller{ model };

1d09993963a90b0c8395b28d3a0f805df81b8ceb
theseChains <rasim0103@gmail.com>
2024-10-10 20:47:03 +0300

    Add constants file

---
+++ 7th-semester/systems-programming/1st-lab/src/Constants.h
@@ -0,0 +1,9 @@
+#ifndef SYSTEMS_PROGRAMMING_CONSTANTS_H_
+#define SYSTEMS_PROGRAMMING_CONSTANTS_H_
+
+namespace systems_programming::constants
+{
+inline constexpr int kMaxAddress{ 16777216 };
+}
+
+#endif
